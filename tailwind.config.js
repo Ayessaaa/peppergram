5e48550f2e716a2be38cb8fc6e90f80a31159773
Ayessa <ayessamaedeguzman78@gmail.com>
2024-11-22 14:06:52 +0800

    Profile created

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "brown1": "#ede0d4",
+        "brown2": "#e6ccb2",
+        "brown3": "#ddb892",
+        "brown4": "#b08968",
+        "brown5": "#7f5539",
+        "brown6": "#9c6644",
+      },
+    },
+  },
+  plugins: [],
+};
